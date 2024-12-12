35395e5b7af7d2b0264087e6ed3cda9f256d4ee0
Kristina Faye <hellokristinafaye@gmail.com>
2024-12-11 23:08:44 -0800

    folder structure done

---
+++ src/components/TitleCards/TitleCards.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './TitleCards.css'
+
+const TitleCards = () => {
+  return (
+      <div className='titlecards'>
+          Title Cards
+    </div>
+  )
+}
+
+export default TitleCards
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,7 +4,7 @@ import './Home.css';
 const Home = () => {
   return (
       <div className='home'>
-          hello
+          Home
     </div>
   )
 }
